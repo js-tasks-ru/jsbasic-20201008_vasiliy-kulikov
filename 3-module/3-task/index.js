9a6af6030c58058bc7c376b5d1cceaf753acd744
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-10-16 17:11:14 +0500

    module-3 task-3 decision

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,15 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  const array = str.split('-');
+  let result = [];
+  result.push(array[0]);
+
+  for (let i = 1; i < array.length; i++) { 
+    let element = array[i];
+    element = element[0].toUpperCase() + element.slice(1);
+    result.push(element);
+  }
+
+  return result.join('');
 }
