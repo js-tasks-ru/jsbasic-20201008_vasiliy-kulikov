940018b75e9597386d56dbc1ac4ebc82ec86d96e
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-10-26 17:13:12 +0500

    module-4 task-2 decision

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,8 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    console.log(table.rows.length);
+    for (let i = 0; i < table.rows.length; i++) { 
+        table.rows[i].cells[i].style.backgroundColor = 'red';
+    }
 }
