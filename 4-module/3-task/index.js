1216c6799ad5a015685b6c5c09801dd443013f84
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-10-26 18:54:55 +0500

    module-4 task-3 decision

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,19 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let tbody = table.querySelector('tbody');
+    for (let row of tbody.rows) {
+
+        if (+row.cells[1].textContent < 18) row.style.textDecoration = 'line-through';
+
+        (row.cells[2].textContent === 'm') ? row.classList.add('male') : row.classList.add('female');
+
+        if (row.cells[3].dataset.available === "true") {
+            row.classList.add('available');
+        } else if (row.cells[3].dataset.available === "false") {
+            row.classList.add('unavailable');
+        } else { 
+            row.hidden = true;
+        }
+    }
 }
