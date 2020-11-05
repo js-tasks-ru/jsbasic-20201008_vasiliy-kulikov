123645b53b6bb43ed697f928066fd6961f1410d1
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-11-05 22:17:07 +0500

    module-6 desicions

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,47 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let rowsArray = rows.map((item) => {
+      let row = `
+        <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</button></td>
+        </tr>
+      `;
+      return row;
+    });
+
+    let table = `
+        <table>
+          <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            ${rowsArray.join('')}
+          </tbody>
+        </table>
+    `;
+
+    let block = document.createElement('div');
+    block.classList.add('table');
+    block.innerHTML = table;
+    block.addEventListener('click', (evt) => { 
+      if (evt.target.tagName === 'BUTTON') { 
+        let trForDelete = evt.target.closest('tr');
+        trForDelete.remove();
+      }
+    });
+    
+    this.elem = block;
   }
+
 }
