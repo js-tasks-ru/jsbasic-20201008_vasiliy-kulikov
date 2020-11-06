cbadff62f35a846dc0a6a518a389bca3081a1f9b
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-11-06 20:03:47 +0500

    module-6 all tasks

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,9 +3,9 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
-    this.slidesHtmlArray = slides.map((item) => {
+    const slidesHtmlArray = slides.map((item) => {
       return `
-        <div class="carousel__slide" data-id="penang-shrimp">
+        <div class="carousel__slide" data-id=${item.id}>
           <img src="/assets/images/carousel/${item.image}" class="carousel__img" alt="slide">
           <div class="carousel__caption">
             <span class="carousel__price">€${item.price.toFixed(2)}</span>
@@ -17,55 +17,72 @@ export default class Carousel {
         </div>
       `
     });
-
-    const slidesBlock = document.createElement('div');
-    slidesBlock.classList.add('carousel');
-    slidesBlock.innerHTML = `
+    this.elem = document.createElement('div');
+    this.elem.classList.add('carousel');
+    this.elem.innerHTML = `
       <div class="carousel__arrow carousel__arrow_right">
         <img src="/assets/images/icons/angle-icon.svg" alt="icon">
       </div>
-      <div class="carousel__arrow carousel__arrow_left">
+      <div class="carousel__arrow carousel__arrow_left" style="display: none">
         <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
       </div>
 
       <div class="carousel__inner">
-        ${this.slidesHtmlArray.join('')}
+        ${slidesHtmlArray.join('')}
       </div>
     `;
     
-    const carouselArrowLeft = slidesBlock.querySelector('.carousel__arrow_left');
-    const carouselArrowRight = slidesBlock.querySelector('.carousel__arrow_right');
-    const slidesContainer = slidesBlock.querySelector('.carousel__inner');
-    const slideImgWidth = slidesContainer.offsetWidth; //Вот здесь пишет что ширина элемента 0
-    const slidesArray = slidesBlock.querySelectorAll('.carousel__slide');
-    const slidesQuantity = slidesArray.length;
-    carouselArrowLeft.style.display = 'none';
-    let position = 0;
+    this.slidesContainer = this.elem.querySelector('.carousel__inner');
+    this.slidesQuantity = this.slides.length;
+    this.position = 0;
+
+    this.arrowLeft = this.elem.querySelector('.carousel__arrow_left');
+    this.arrowRight = this.elem.querySelector('.carousel__arrow_right');
+
+    this.arrowRight.addEventListener('click', () => { this.clickRight() });
+    this.arrowLeft.addEventListener('click', () => { this.clickLeft() });
+
+
+    let buttonOrders = this.elem.querySelectorAll('.carousel__button');
+    for (let button of buttonOrders) { 
+      button.addEventListener('click', () => { 
+        this.order(button); // придумал только с передачей элемента в виде параметра
+      });
+    }
+  }
+
+  clickRight() {
+    this.slideImgWidth = this.slidesContainer.offsetWidth;
+    this.position -= this.slideImgWidth;
+    this.slidesContainer.style.transform = `translateX(${this.position}px)`;
+
+    if (-this.position === (this.slidesQuantity - 1) * this.slideImgWidth) {
+      this.arrowRight.style.display = 'none';
+    } else {
+      this.arrowLeft.style.display = '';
+    }
+  }
   
-    carouselArrowLeft.addEventListener('cslidesBlocklick', () => {
-      position += slideImgWidth;
-      slidesContainer.style.transform = `translateX(${position}px)`;
-  
-      if (position === 0) {
-        carouselArrowLeft.style.display = 'none';
-      } else {
-        carouselArrowRight.style.display = '';
-      }
-    })
-  
-    carouselArrowRight.addEventListener('click', () => {
-      position -= slideImgWidth;
-      slidesContainer.style.transform = `translateX(${position}px)`;
-  
-      if (-position === (slidesQuantity - 1) * slideImgWidth) {
-        carouselArrowRight.style.display = 'none';
-      } else {
-        carouselArrowLeft.style.display = '';
-      }
-    })
-    
-    this.elem = slidesBlock;
-    
-    
+  clickLeft() { 
+    this.position += this.slideImgWidth;
+    this.slidesContainer.style.transform = `translateX(${this.position}px)`;
+
+    if (this.position === 0) {
+      this.arrowLeft.style.display = 'none';
+    } else {
+      this.arrowRight.style.display = '';
+    }
+  }
+
+  order(button) { // возможно по-правильному это должно быть реализовано иначе, но не придумал как
+
+    let slideId = button.closest('.carousel__slide').dataset.id;
+
+    let customEvent = new CustomEvent("product-add", {
+      detail: slideId,
+      bubbles: true
+    });
+
+    this.elem.dispatchEvent(customEvent);
   }
 }
