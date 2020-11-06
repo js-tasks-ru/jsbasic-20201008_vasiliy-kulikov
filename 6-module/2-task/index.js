eeb07390744f8567bd442dad8237f98c6803d9eb
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-11-06 11:53:42 +0500

    module-6 task-2 decision

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,34 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+    `;
+
+    const button = card.querySelector('.card__button');
+    button.addEventListener('click', () => { 
+      const customEvent = new CustomEvent("product-add", { // имя события должно быть именно "product-add"
+        detail: product.id, // Уникальный идентификатора товара из объекта товара
+        bubbles: true // это событие всплывает - это понадобится в дальнейшем
+      });
+      button.dispatchEvent(customEvent);
+    })
+
+    card.addEventListener('product-add', () => {
+
+    });
+
+    this.elem = card;
   }
 }
