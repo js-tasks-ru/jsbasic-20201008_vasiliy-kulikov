d2bc59f24c640d9fe35bee0ab807f52a3b6d367b
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-11-09 15:48:21 +0500

    module-7

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -4,7 +4,7 @@ export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
     
-    let links = categories.map((item) => {
+    let linksHtml = categories.map((item) => {
       if (item.name === 'All') { 
         return `<a href="#" class="ribbon__item ribbon__item_active" data-id="${item.id}">${item.name}</a>`;
       }
@@ -14,15 +14,15 @@ export default class RibbonMenu {
     });
 
     let menuHtml = `
-        <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+        <button class="ribbon__arrow ribbon__arrow_left">
           <img src="/assets/images/icons/angle-icon.svg" alt="icon">
         </button>
 
         <nav class="ribbon__inner">
-          ${links.join('')}
+          ${linksHtml.join('')}
         </nav>
 
-        <button class="ribbon__arrow ribbon__arrow_right">
+        <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
           <img src="/assets/images/icons/angle-icon.svg" alt="icon">
         </button>
     `;
@@ -35,10 +35,52 @@ export default class RibbonMenu {
 
     this.buttonLeft = this.elem.querySelector('.ribbon__arrow_left');
     this.buttonRight = this.elem.querySelector('.ribbon__arrow_right');
+
     this.buttonLeft.addEventListener('click', () => this.clickLeft());
     this.buttonRight.addEventListener('click', () => this.clickRight());
+
     this.ribbonInner = this.elem.querySelector('.ribbon__inner');
-    console.log(this.ribbonInner);
+
+    this.ribbonInner.onscroll = () => { 
+      let ribbonClientWidth = this.ribbonInner.clientWidth;
+      let ribbonScrollLeft = this.ribbonInner.scrollLeft;
+      let ribbonScrollWidth = this.ribbonInner.scrollWidth;
+
+      let ribbonScrollRight = ribbonScrollWidth - ribbonScrollLeft - ribbonClientWidth;
+
+      if (ribbonScrollLeft > 0) {
+        this.buttonLeft.classList.add('ribbon__arrow_visible');
+      }
+
+      if (ribbonScrollLeft === 0) { 
+        this.buttonLeft.classList.remove('ribbon__arrow_visible');
+      }
+
+      if (ribbonScrollRight > 0) { 
+        this.buttonRight.classList.add('ribbon__arrow_visible');
+      }
+
+      if (ribbonScrollRight === 0) { 
+        this.buttonRight.classList.remove('ribbon__arrow_visible');
+      }
+
+    }
+
+    let links = menu.querySelectorAll('.ribbon__item');
+    
+    for (let link of links) { 
+      link.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        for (let item of links) { 
+          item.classList.remove('ribbon__item_active');
+        }
+
+        link.classList.add('ribbon__item_active');
+
+        this.filterCategory(link);
+      })
+    }
   }
   
   clickLeft() { 
@@ -46,6 +88,17 @@ export default class RibbonMenu {
   }
 
   clickRight() { 
-    this.ribbonInner.scrollBy(350, 0);
+    this.ribbonInner.scrollBy(350, 0);  
+  }
+
+  filterCategory(link) { 
+    let categoryId = link.dataset.id;
+
+    let customEvent = new CustomEvent('ribbon-select', {
+      detail: categoryId,
+      bubbles: true
+    });
+
+    this.elem.dispatchEvent(customEvent);
   }
 }
