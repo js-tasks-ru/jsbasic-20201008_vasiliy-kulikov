0755b00f0225e6af8746336161343c6693bf28bc
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2020-11-16 22:23:10 +0500

    module 9

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -16,6 +16,51 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    this.carousel = new Carousel(slides);
+    this.carouselContainer = document.body.querySelector('[data-carousel-holder]');
+    this.carouselContainer.append(this.carousel.elem);
+
+    this.ribbonMenu = new RibbonMenu(categories);
+    this.ribbonMenuContainer = document.body.querySelector('[data-ribbon-holder]');
+    this.ribbonMenuContainer.append(this.ribbonMenu.elem);
+
+    this.stepSlider = new StepSlider({steps: 5, value: 3});
+    this.stepSliderContainer = document.body.querySelector('[data-slider-holder]');
+    this.stepSliderContainer.append(this.stepSlider.elem);
+
+    this.cartIcon = new CartIcon();
+    this.cartIconContainer = document.body.querySelector('[data-cart-icon-holder]');
+    this.cartIconContainer.append(this.cartIcon.elem);
+
+    this.cart = new Cart(this.cartIcon);
+
+    let response = await fetch('products.json');
+
+    if (response.ok) {
+      this.products = await response.json();
+      this.productsGrid = new ProductsGrid(this.products);
+      this.productsGridContainer = document.body.querySelector('[data-products-grid-holder]');
+      this.productsGridContainer.innerHTML = "";
+      this.productsGridContainer.append(this.productsGrid.elem);
+    } else { 
+      console.error('Ошибка загрузки товаров с сервера: ' + response.status);
+    }
+
+    this.productsGrid.updateFilter({
+      noNuts: document.getElementById('nuts-checkbox').checked,
+      vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
+      maxSpiciness: this.stepSlider.value,
+      category: this.ribbonMenu.value
+    });
+
+    this.addEventListeners();
+
+
+  }
+
+  addEventListeners() { 
+    document.body.addEventListener('product-add', (evt) => this.cart.addProduct(this.products.find(item => item.id === evt.detail)));
+    document.body.querySelector('.slider').addEventListener('slider-change', () => this.productsGrid.updateFilter());
+    document.body.querySelector('.ribbon').addEventListener('ribbon-select', () => this.productsGrid.updateFilter());
   }
 }
